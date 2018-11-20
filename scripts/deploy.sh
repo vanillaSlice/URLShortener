3e19be0cff56ee44d337467af66e8f83a960c5c1
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-11-20 21:41:57 +0000

    Remove heroku run command from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-docker login -u _ -p "$HEROKU_API_KEY"  registry.heroku.com
+docker login --username=_ --password="$HEROKU_API_KEY" registry.heroku.com
 docker build -t registry.heroku.com/sliceurl/web -f Dockerfile .
 docker push registry.heroku.com/sliceurl/web
-heroku run --app sliceurl
 image_id=$(docker inspect registry.heroku.com/sliceurl/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$image_id"'"}]}'
 curl -n -X PATCH https://api.heroku.com/apps/sliceurl/formation -d "$payload" -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer $HEROKU_API_KEY"
